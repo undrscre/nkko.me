28be102e2efe90a868240d7bb9f9e8fa35b1bc35
nicholas g. <105597411+nkkls@users.noreply.github.com>
2024-09-07 14:23:42 +0800

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<div style="display: flex; width:100vw; justify-content:center; align-items:center; height:100vh; gap:3em;">
+    <div>
+        <h1 class="font-pona" style="font-size: 5em; margin:0; text-align:right;">
+            mi pali e lipu
+        </h1>
+        <p style="margin:0; font-size:1.5em; text-align:right;"><b>website under construction, AGAIN.</b><br>
+        <a style="color: #f08248;" href="https://lethallava.land/@niko">fedi</a> / 
+        <a style="color: turquoise;" href="https://github.com/undrscre">github</a> / 
+        <a style="color: #a855f7;" href="https://github.com/undrscre/nkko.me">repository</a></p>
+    </div>
+
+    <h1 class="font-pona" style="font-size: 8em; margin:0; text-align:right;">
+        suwi /
+    </h1>
+</div>
