fd044fa7483de39a0b9ec95e3f03415b3146c9f0
undrscre <105597411+undrscre@users.noreply.github.com>
2024-11-12 04:01:37 +0000

    yea

---
+++ src/routes/update/+page.svelte
@@ -0,0 +1,3 @@
+<div style="display: flex; width:100vw; justify-content:center; align-items:center; height:100vh; gap:3em;">
+    <iframe width="1120" height="630" src="https://www.youtube-nocookie.com/embed/hq5pg_zewMM?si=nsBC39RRWpYLMPpx&amp;controls=0&amp;start=619&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+</div>
