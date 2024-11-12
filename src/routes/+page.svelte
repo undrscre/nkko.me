b6e245a586f8393c3cbd73167388b4396cfb0736
undrscre <105597411+undrscre@users.noreply.github.com>
2024-11-12 03:59:26 +0000

    yeah!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,3 @@
 <div style="display: flex; width:100vw; justify-content:center; align-items:center; height:100vh; gap:3em;">
-    <div>
-        <h1 class="font-pona" style="font-size: 5em; margin:0; text-align:right;">
-            soweli mi pali e lipu
-        </h1>
-        <p style="margin:0; font-size:1.5em; text-align:right;"><b>website under construction, AGAIN.</b><br>
-        <!-- <a style="color: #f08248;" href="https://grimgreenfo.rest/@">fedi</a> / -->
-        <a style="color: turquoise;" href="https://github.com/undrscre">github</a> / 
-        <a style="color: #a855f7;" href="https://github.com/undrscre/nkko.me">repository</a></p>
-    </div>
-
-    <h1 class="font-pona" style="font-size: 8em; margin:0; text-align:right;">
-        suwi /
-    </h1>
+    <a href="/update">click here to enter</a>
 </div>
